842efd0b2dd5e29ff2fbbbeb86ca6e46c3ec531b
alexmhoule <alexmhoule@gmail.com>
2015-08-17 17:50:18 -0500

    initial push

---
+++ js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+	$('#submit').click(function () {
+		if($('#search-Term').val().length!=0){
+			loadAjax();
+		} else {
+			alert("Please enter something to search for");
+		}
+	});
+
+	function loadAjax() {
+	
+		var data = {
+			part: 'snippet',
+			key: 'AIzaSyBg6j7YewqXfEbWEQxTW7CoAOh72QMkSTA',
+			maxResults: 50,
+			q: $('#search-Term').val()
+		};
+
+		$.ajax({
+			url: "https://www.googleapis.com/youtube/v3/search",
+			data: data
+		}).done(function (results) {
+			console.log(results); /* Display YouTube - Hint check quiz app just like loading quiz data - */
+		}).error(function (error) {
+			console.log(error);
+		});
+
+	}
+	
+});
